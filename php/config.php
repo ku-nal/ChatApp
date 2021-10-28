45f431a964ae5cd4c46c82ce3d0adac4ff59a7ec
Kunal Makwana <48153221+ku-nal@users.noreply.github.com>
2021-10-28 10:42:40 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -2,9 +2,9 @@
   $access_key_id="AKIA2522ZOBSAXXIJOU4";
   $secret_access_key="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";
   
-  $hostname = "database-1.cd7o12fmcmka.ap-south-1.rds.amazonaws.com";
-  $username = "kunal";
-  $password = "kunal1234";
+  $hostname = "database-1.cnpfftxfe9y8.ap-south-1.rds.amazonaws.com";
+  $username = "admin";
+  $password = "admin123";
   $dbname = "chat";
   $conn = mysqli_connect($hostname, $username,$password, $dbname);
   if(!$conn){
